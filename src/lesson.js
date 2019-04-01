2c479dfe8cf683b038248941fb96c4b6368bbebe
noah harvey <noah.harvey@generalassemb.ly>
2019-04-01 14:56:08 -0400

    moves lesson code to its own module

---
+++ src/lesson.js
@@ -0,0 +1,87 @@
+//NOTE: since 2019-03-18, fs.promises is experimental
+const fs = require('fs');
+
+export function Lesson(sequence = new Sequence(0,0,0,1), name = 'TBD', url = '', depends = []) {
+  this.sequence = sequence; 
+  this.name = name;
+  this.url = url;
+  this.depends = depends;
+
+  this.compareLessonsBySequence = function(b) {
+    return this.sequence.compareSequence(b.sequence);
+  }
+};
+
+export const lessonCompare = (a,b) => a.compareLessonsBySequence(b);
+
+export const parseLessonsFromJSON = (calendarJSON) => { 
+  let lessons = JSON.parse(calendarJSON);
+  return lessons.map(l => new Lesson(makeSequenceFromString(l.sequence), l.name, l.url, l.depends));
+
+};
+
+//reads calendar file. Transparent promise wrapper around readFile
+export const readLessonsFile = (filePath) => 
+  new Promise(function(resolve, reject) {
+    fs.readFile(filePath, (err, data) => {
+      if(err) {
+        reject(err)
+      } else {
+
+        resolve(parseLessonsFromJSON(data));
+      }
+    });
+  });
+
+export function Sequence(unit, day, block, subblock) {
+    this.unit = unit;
+    this.day = day; 
+    this.block = block;
+    this.subblock = subblock;
+
+    this.toString = function() {
+      return makeSequenceString(this.unit, this.day, this.block, this.subblock);
+    }
+
+    this.compareSequence = function(b) {
+      return Math.sign(
+        8*Math.sign(this.unit - b.unit)
+        + 4*Math.sign(this.day - b.day)
+        + 2*Math.sign(this.block - b.block)
+        + Math.sign(this.subblock - b.subblock)
+      );
+    }
+}
+
+export function SequenceFormatException(str) {
+  str = str.trim();
+
+  this.value = str;
+  this.message = 'sequence should match /^(\d+).(\d+).(\d+)$/';
+  this.toString = function () { this.value + ' ' + this.message };
+}
+
+export const makeSequenceFromString = (str) => {
+  let pattern = /^(\d+).(\d+).(\d+)(.(\d+))?$/
+
+  let result = pattern.exec(str);
+
+  if(result)
+  {
+    return new Sequence(Number.parseInt(result[1], 10),
+      Number.parseInt(result[2], 10),
+      Number.parseInt(result[3], 10),
+      result[5] ? Number.parseInt(result[5], 10) : 1
+    );
+  }
+  else
+  {
+    throw new SequenceFormatException(str); 
+  }
+}
+
+export const makeSequenceString = (unit, day, block, subblock = 1) => {
+  return `${unit}.${day}.${block}.${subblock}`;
+}
+
+export const sequenceCompare = (a,b) => a.compareSequence(b);
