244c1e3a4165b6ecdb199f88641b0659dfb392a2
noah harvey <noah.harvey@generalassemb.ly>
2019-04-03 18:44:28 -0400

    changes gen script to use fish shell

--- src/makeMono.js
+++ src/makeMono.js
@@ -2,36 +2,33 @@ const { exec } = require("child_process");
 const { readLessonsFile, jsonFriendlyLesson } = require('./lesson.js');
 const { makeAllLessons, lessonsToMarkDownTable } = require('./scheduler.js');
 
-const cloneLesson = (lesson) => {
-  return new Promise((resolve, reject) => {
-    exec(`echo 'git clone ${lesson.url} bin/${lesson.name}'`, (err, stdout, stderr) => {
-      
-      console.error(stderr);
+const makeLessonURLLocal = (lesson) => {
+  lesson.url = `./${lesson.name}`;
 
-      if(err) {
-        console.error("Saving to new dir");
-
-        exec(`mkdir -p bin/${lesson.name}`);
-        exec(`echo \"# ${lesson.name}\n\n${lesson.url}\" >> bin/${lesson.name}/readme.md`);
-      }
-
-      lesson.url = `./${lesson.name}`;
-
-      resolve(lesson);
-    });
-  });
+  return lesson;
 };
 
-exec('mkdir -p bin');
+const isGitLesson = (lesson) => 
+  lesson.url.indexOf("git") !== -1
+  && lesson.url.indexOf("/master") === -1;
+
+const printGitClone = (lesson) => `git ${lesson.name} ${lesson.url}`;
+
+const printNonGit = (lesson) => `dir ${lesson.name} ${lesson.url}`;
+
+const printLessonAction = (lesson) => isGitLesson(lesson) 
+  ? printGitClone(lesson) 
+  : printNonGit(lesson);
 
 readLessonsFile(process.argv[2])
-  .then(lessons => Promise.all(
-    lessons.filter(l => l.sequence.unit == 1 || l.sequence.unit == 2).map(cloneLesson)
-    //lessons.map(cloneLesson)
-  )).then(newLessons => {
+  .then(ll => {
+    let lessons = ll.filter(l => l.sequence.unit == 1 || l.sequence.unit == 2)
+
+    lessons.forEach(l => console.error(printLessonAction(l)));
+
     console.log(
       lessonsToMarkDownTable(
-        makeAllLessons(newLessons).map(jsonFriendlyLesson)
+        makeAllLessons(lessons.map(makeLessonURLLocal)).map(jsonFriendlyLesson)
       )
     );
   });
