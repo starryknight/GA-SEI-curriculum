a4f21cc8ae6d7cb4d4deb1e0b38580440b017fa3
noah harvey <noah.harvey@generalassemb.ly>
2019-04-11 11:54:58 -0400

    updates printLessonNames to use new sequences

--- src/printLessonNames.js
+++ src/printLessonNames.js
@@ -4,3 +4,8 @@ let scheduleJSONFilePath = process.argv[2];
 
 if(scheduleJSONFilePath)
   readScheduleJSONFile(scheduleJSONFilePath)
+    .then(sequences => {
+      console.log(sequences.map(s => s.toString()))
+    });
+else
+  console.error("schedule json file required!")
