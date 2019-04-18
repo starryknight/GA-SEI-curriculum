2595bc8a672196cac6743f25bd79f6a6b3305ca6
noah harvey <noah.harvey@generalassemb.ly>
2019-04-18 15:48:58 -0400

    creating homework

--- sequences/express-sample-project/mongo/connection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost/bank', { useNewUrlParser: true }).then(() => {
-    console.log('MONGODB is now connected')
-})
-
-module.exports = mongoose
---
+++ sequences/express-views-exercise/coffee-shop/server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const bankApi = require('./api/bankApi.js')
+
+// Register middleware
+app.use(express.json())
+
+//add middleware for handlebars here
+
+//global variable to store list of accounts
+let accounts = [];
+
+//Write your HTTP request handlers using RESTful routes here
+//call methods in the bankApi as needed. Feel free to modify the API
+//as you see fit to accomplish the goals of the app
+
+//accounts GET (all) send back a page with all of the accounts listed (only
+//show their names and balances)
+app.get("", (req, res) => { });
+
+//accounts GET (single)
+//send back a single page with the details of a single acount displayed
+app.get("", (req, res) => {
+});
+
+//accounts POST
+//this should add a new POST from req.body to accounts (global variable)
+//and sends back the same page to list all accounts.
+app.get("", (req, res) => {
+});
+
+//accounts PUT (note here you'll need to put /put at the end of your
+//path. This is a work around because HTML forms only allow GET and POST
+//requests). Make sure the HTML Form has the pattern: action='.../put' 
+app.get("", (req, res) => {
+});
+
+//accounts DELETE (note here you'll need to put /delete at the end of your
+//path. This is a work around because HTML forms only allow GET and POST
+//requests). Make sure the HTML Form has the pattern: action='.../put' 
+app.get("", (req, res) => { 
+});
+
+//keep these lines at the bottom of the file
+const PORT = process.env.PORT || 3000 
+
+app.listen(PORT, () => {
+    console.log(`App is listening on PORT ${PORT}`)
+})
