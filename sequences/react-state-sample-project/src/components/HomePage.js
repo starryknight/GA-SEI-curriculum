213498c04bc78fcb20fd73ac2b0d38a3705f912d
noah harvey <noah.harvey@generalassemb.ly>
2019-04-30 12:56:01 -0400

    makes react-state-sample-project classroom ready

--- sequences/react-state-sample-project/src/components/HomePage.js
+++ sequences/react-state-sample-project/src/components/HomePage.js
@@ -2,17 +2,13 @@ import React, {Component} from 'react'
 
 import AdminView from './AdminView'
 import ShopView from './ShopView'
-import CartView from './CartView'
+/* Import CartView component here */
 
 class HomePage extends Component {
   //add class instance state member
 
   toggleEditSaleItem = () => {
     //toggle state's showEditSaleItem boolean
-  };
-
-  toggleAdminView = () => {
-    //toggle state's showAdminView  k
   };
 
   handleItemCurrentlyOnSaleChange = (event) => {
@@ -22,82 +18,61 @@ class HomePage extends Component {
 
   addNewProductToProductList = (newProduct) => {
     //get state's product list
-    productList.push(newProduct)
+
+    //add product to product list
+    //productList.push(newProduct)
+
     //update state's product list
   };
 
   deleteProductFromListByIndex = (productToDelete) => {
     //get state's product list
-    productList.splice(productToDelete, 1)
+
+    //remove product from list
+    //productList.splice(productToDelete, 1)
+
     //update state's product list
-  };
-
-  addProductToCart = (index) => {
-    //get product list at index from state
-    //get state's cart list
-
-    cartList.push(product)
-
-    //update state's cart list
-  };
-
-  removeProductFromCart = (index) => {
-    //get state's cart list
-    cartList.splice(index, 1)
-
-    //update state's cartlist
   };
 
   render () {
     const adminView = <AdminView
-      productList={//state's product list}
+      productList={[]}
       addNewProductToProductList={this.addNewProductToProductList}
-      deleteProductFromListByIndex={this.deleteProductFromListByIndex}/>
-
-    const shopView = <ShopView
-      productList={//get state's product list}
-      addProductToCart={this.addProductToCart}/>
+      deleteProductFromListByIndex={this.deleteProductFromListByIndex}
+      />
 
     return (
       <div>
         <div>
           <div id="home-page-nav">
             <h1>Hardware Store</h1>
-            <span>Currently On Sale: {//state's item currently on sale}!</span>
+            <span>Currently On Sale: {/* state's item currently on sale*/ }!</span>
 
             <div>
               {
-                /* state's edit sale item boolean */ ? <div>
+                 <div>
                   <input
                     onChange={this.handleItemCurrentlyOnSaleChange}
-                    value={//state's item currently on sale}
                     type="text"
                   />
                 </div>
-                  : null
               }
             </div>
             <div>
               <button onClick={this.toggleEditSaleItem}>
-                {//state's show edit boolean
-                  ? 'Hide'
-                  : 'Edit Sale Item'}
+                  'Edit Sale Item'
               </button>
             </div>
             <div>
               <button onClick={this.toggleAdminView}>
-                {//state's showAdminView boolean
-                  ? 'Show Shop View'
-                  : 'Show Admin View'}
+                  'Show Admin View'
               </button>
             </div>
           </div>
         </div>
 
         <div id="view-container">
-          {/* state's showAdminView boolean */ ? adminView : shopView}
-
-          /* Call CartView Component Here */
+          { adminView }
         </div>
       </div>
     )
